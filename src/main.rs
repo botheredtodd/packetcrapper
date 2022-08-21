9e5ed135493dcf5a1fe0cde39ab30c2c606b4a29
Todd Balsley <todd.balsley@cox.com>
2022-08-21 12:05:06 -0400

    Alright, you have to add the dhcp message type as an option in addition to setting it as a flag.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::net::{Ipv4Addr, UdpSocket};
 use std::str::FromStr;
 use dhcproto::{v4, Encodable, Encoder};
 use dhcproto::v4::{relay::{RelayInfo, RelayAgentInformation}};
-
+use v4::OptionCode::MessageType;
 
 
 fn main() {
@@ -11,10 +11,16 @@ fn main() {
     let chaddr = vec![
         29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
     ];
+    let chaddr2 = vec![
+        29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 94,
+    ];
+    let giaddr = Ipv4Addr::from_str("192.168.2.145").unwrap();
 // construct a new Message
     let mut msg = v4::Message::default();
     msg.set_flags(v4::Flags::default().set_broadcast()) // set broadcast to true
         .set_chaddr(&chaddr) // set chaddr
+        .set_giaddr(giaddr)
+        .set_htype(dhcproto::v4::HType::Eth)
         .opts_mut()
         .insert(v4::DhcpOption::MessageType(v4::MessageType::Discover)); // set msg type
 
@@ -28,15 +34,16 @@ fn main() {
         ]));
     msg.opts_mut()
         .insert(v4::DhcpOption::ClientIdentifier(chaddr));
-    let stewwie = Ipv4Addr::from_str("192.168.2.145").unwrap();
-    msg.opts_mut()
-        .insert(v4::DhcpOption::Router(vec![stewwie]));
+
 
     let mut info = RelayAgentInformation::default();
     info.insert(RelayInfo::LinkSelection("1.2.3.4".parse().unwrap()));
+    info.insert(RelayInfo::AgentRemoteId(chaddr2));
     let mut msg = v4::Message::default();
     msg.opts_mut()
         .insert(v4::DhcpOption::RelayAgentInformation(info));
+    msg.opts_mut()
+        .insert(v4::DhcpOption::MessageType(v4::MessageType::Discover));
 
     
 
@@ -47,7 +54,7 @@ fn main() {
     let mut e = Encoder::new(&mut buf);
     msg.encode(&mut e);
 
-    let socket = UdpSocket::bind("192.168.2.145:24567").expect("couldn't bind to address");
+    let socket = UdpSocket::bind("192.168.2.145:68").expect("couldn't bind to address");
     socket.connect("192.168.2.221:67").expect("TODO: panic message");
     socket.send(&buf).expect("couldn't send data");
 // buf now has the contents of the encoded DHCP message
